249b7a8189fe47d27d21058173929ea6c56fa5b8
Anchit Sharma <anchit@Anchits-MacBook-Pro.local>
2019-10-18 23:43:49 -0500

    pipeline skeleton

---
+++ check-hbase.py
@@ -0,0 +1,14 @@
+
+
+import happybase
+connection = happybase.Connection('0.0.0.0', port=9090)
+
+
+table = connection.table('articles')
+
+#table.put(b'test-key3',{b'cf1:col':b'bam!'})
+
+rows = table.rows(['id14174id'])
+for key, data in rows:
+    print(key, data)
+
--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-'''import pyspark
+
+import pyspark
+import xml.etree.ElementTree as ET
 from pyspark import SparkContext
-sc =SparkContext()
-
-nums= sc.parallelize([1,2,3,4])
-nums.take(1)
-squared = nums.map(lambda x: x*x).collect()
-for num in squared:
-    print('%i ' % (num))
-'''
-
-
 import happybase
-connection = happybase.Connection('0.0.0.0', port=9090)
 
 
-table = connection.table('articles')
+sc =SparkContext()
+files='file:///Users/anchit/Desktop/test_dir/enwiki-20190920-pages-articles-multistream1.xml-p10p30302'
 
-#table.put(b'test-key2',{b'cf1:col':b'bam!'})
 
-rows = table.rows([b'test-key',b'test-key2'])
-for key, data in rows:
-    print(key, data)
+## Util functions
+def split_str(x):
+    x=x[1]
+    x=x[x.find('<page>'):]
+    return x+'</page>'
+
+
+def parse_xml(x):
+    ret=None
+    try:
+        root=ET.fromstring(x)
+    except:
+        print(x)
+        return None
+
+    id=root.findall('.//id')[0]
+    id=ET.tostring(id).decode()
+    id=''.join(e for e in id if e.isalnum())
+
+    text=root.findall('.//text')
+    text=[ET.tostring(t).decode() for t in text]
+    text=' '.join(text)
+    text=''.join(e for e in text if e.isalnum())
+    return id,text
+
+
+def write_hbase(x):
+    
+    if x != None:
+        print('key')
+        print(x[0])
+        connection = happybase.Connection('0.0.0.0', port=9090)
+        table = connection.table('articles')
+        #table.put(b'spark',{b'cf1:col':str(x)})
+        table.put(x[0],{b'cf1:col':x[1]})
+
+
+## call and run
+file_rdds=sc.newAPIHadoopFile(files, "org.apache.hadoop.mapreduce.lib.input.TextInputFormat",
+                    "org.apache.hadoop.io.LongWritable", "org.apache.hadoop.io.Text",
+                    conf={"textinputformat.record.delimiter": "</page>"})\
+.map(split_str)\
+.map(parse_xml)\
+.foreach(write_hbase)
 
