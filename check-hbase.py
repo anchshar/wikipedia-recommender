0870b889267a3c7b6592483fa124b353e04779f9
Anchit Sharma <anchit@er-10-131-166-194.wireless.umn.edu>
2019-10-23 14:30:05 -0500

    Added multiple col family handling

--- check-hbase.py
+++ check-hbase.py
@@ -8,7 +8,7 @@ table = connection.table('articles')
 
 #table.put(b'test-key3',{b'cf1:col':b'bam!'})
 
-rows = table.rows(['id14174id'])
+rows = table.rows(['id11712id'])
 for key, data in rows:
     print(key, data)
 
--- main.py
+++ main.py
@@ -3,10 +3,21 @@ import pyspark
 import xml.etree.ElementTree as ET
 from pyspark import SparkContext
 import happybase
+import re
 
 
 sc =SparkContext()
 files='file:///Users/anchit/Desktop/test_dir/enwiki-20190920-pages-articles-multistream1.xml-p10p30302'
+
+table_name='articles'
+
+user_family='user_cf'
+user_pref='username_'
+
+article_family='article_cf'
+article_pref='article'
+
+content_loc='cf1:content'
 
 
 ## Util functions
@@ -27,23 +38,63 @@ def parse_xml(x):
     id=root.findall('.//id')[0]
     id=ET.tostring(id).decode()
     id=''.join(e for e in id if e.isalnum())
+    id=id[2:len(id)-2]
+
+    username=root.findall('.//username')
+    if len(username)==0:
+        return None
+    username=username[0]
 
     text=root.findall('.//text')
     text=[ET.tostring(t).decode() for t in text]
     text=' '.join(text)
-    text=''.join(e for e in text if e.isalnum())
-    return id,text
+
+    words=re.split(r'\s+',text)
+    vec={}
+    for word in words:
+        if word in vec:
+            vec[word]=vec[word]+1
+        else:
+            vec[word]=1
+
+    words=[''.join(e for e in word if e.isalnum()) for word in words]
+
+    text=' '.join(words)
+    return {'id':id,'username':username,'text':text,'vec':vec}
 
 
 def write_hbase(x):
     
+    global content_loc
+    
+    global article_family
+    global article_pref
+    
+    
     if x != None:
         print('key')
-        print(x[0])
+        print(x['id'])
+        id=x['id']
+        content=x['text']
+        vec=x['vec']
+
         connection = happybase.Connection('0.0.0.0', port=9090)
-        table = connection.table('articles')
-        #table.put(b'spark',{b'cf1:col':str(x)})
-        table.put(x[0],{b'cf1:col':x[1]})
+        table = connection.table(table_name)
+        
+        # Put full content
+        table.put(article_pref+id,{content_loc:content})
+
+        # Put article vector
+        row=table.row(article_pref+id,columns=[article_family])
+        
+        if key,val in row:
+            word=key[key.find(article_family+':')+1]
+            if word in vec:
+                vec[word]=vec[word]+int(val)
+
+    
+        for word in vec:
+            table.put(article_pref+id,{article_family+':'+word:vec[word]})
 
 
 ## call and run
